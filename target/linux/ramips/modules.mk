a6f158e983b1acc6b5dd90615299feb83890d0c6
Laurent Denizot <laurent@denizot.link>
2020-12-30 15:56:37 +0100

    add PCM 5102A

--- target/linux/ramips/modules.mk
+++ target/linux/ramips/modules.mk
@@ -121,14 +121,14 @@ define KernelPackage/sound-mt7620
 	CONFIG_SND_RALINK_SOC_I2S \
 	CONFIG_SND_SIMPLE_CARD \
 	CONFIG_SND_SIMPLE_CARD_UTILS \
-	CONFIG_SND_SOC_SPDIF
+  	CONFIG_SND_SOC_PCM5102A
+  	
   FILES:= \
 	$(LINUX_DIR)/sound/soc/ralink/snd-soc-ralink-i2s.ko \
 	$(LINUX_DIR)/sound/soc/generic/snd-soc-simple-card.ko \
 	$(LINUX_DIR)/sound/soc/generic/snd-soc-simple-card-utils.ko \
-	$(LINUX_DIR)/sound/soc/codecs/snd-soc-spdif-rx.ko \
-	$(LINUX_DIR)/sound/soc/codecs/snd-soc-spdif-tx.ko
-  AUTOLOAD:=$(call AutoLoad,90,snd-soc-spdif-rx snd-soc-spdif-tx snd-soc-ralink-i2s snd-soc-simple-card)
+	$(LINUX_DIR)/sound/soc/codecs/snd-soc-pcm5102a.ko
+  AUTOLOAD:=$(call AutoLoad,90, snd-soc-pcm5102a snd-soc-ralink-i2s snd-soc-simple-card)
   $(call AddDepends/sound)
 endef
 
